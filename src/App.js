a09ef769f77a3e7cff1a2424cf22d5b00055f815
soycarlosherrera <carlos4505@gmail.com>
2023-12-04 21:43:29 -0500

    update navbar logos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { NavBar } from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/NavBar';
+
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,9 @@ import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon4 from '../assets/img/nav-icon4.svg';
+import logoSoyCarlosHerrera from '../assets/img/logoSoyCarlosHerrera.svg'
+
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -31,7 +34,7 @@ export const NavBar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
             <Container>
                 <Navbar.Brand href="#home">
-                    <img src={logo} alt="Logo" /> 
+                    <img src={logoSoyCarlosHerrera} alt="Logo" /> 
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
@@ -45,8 +48,9 @@ export const NavBar = () => {
                     <span className="navbar-text">
                         <div className="social-icon">
                             <a href="#"><img src={navIcon1} alt=""/></a>
+                            <a href="#"><img src={navIcon4} alt=""/></a> 
                             <a href="#"><img src={navIcon2} alt=""/></a>
-                            <a href="#"><img src={navIcon3} alt=""/></a>
+                            <a href="#"><img src={navIcon3} alt=""/></a>                            
                         </div>
                         <button className="vvc" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
                     </span>
