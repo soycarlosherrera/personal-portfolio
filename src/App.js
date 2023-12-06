945efb985f7b66fa3a14e031ec61b5c94ce3939b
soycarlosherrera <carlos4505@gmail.com>
2023-12-05 22:56:06 -0500

    banner creation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar />  
+      <NavBar />
+      <Banner />  
     </div>
   );
 }
