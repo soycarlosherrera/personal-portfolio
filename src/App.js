b9a32beb518f8903216c55c6b209fc1e9960842b
soycarlosherrera <carlos4505@gmail.com>
2023-12-09 16:39:41 -0500

    Css Contac created

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
-
+import { Contact } from './components/Contact';
 
 function App() {
   return (
@@ -13,7 +13,8 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
-      <Projects />  
+      <Projects />
+      <Contact />  
     </div>
   );
 }
