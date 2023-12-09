45bb029d5377b0b4aed7d3626714490f1a6528b0
soycarlosherrera <carlos4505@gmail.com>
2023-12-09 17:45:20 -0500

    Css Footer Update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      <Contact />  
+      <Contact />
+      <Footer />  
     </div>
   );
 }
