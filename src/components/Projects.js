b20620af59d749682b130f3702e8593c38e53dce
soycarlosherrera <carlos4505@gmail.com>
2023-12-08 23:16:31 -0500

    Contact creation

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -45,13 +45,11 @@ export const Projects = () => {
       <section className="project" id="projects">
         <Container>
           <Row>
-            <Col>
-              
-                
+            <Col>               
                   <h2>Projects</h2>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                    <Nav variant="pills" defaultActiveKey="/home">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                       <Nav.Item>
                         <Nav.Link eventKey="first">Tab 1</Nav.Link>
                       </Nav.Item>
