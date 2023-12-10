43dfbb786eaab472d91691ba54065ee6f11c0711
soycarlosherrera <carlos4505@gmail.com>
2023-12-09 23:26:34 -0500

    Project Completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from '../assets/img/nav-icon4.svg';
 
 export const Footer = () => {
   return (
@@ -16,7 +17,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://linkedin.com/in/soycarlosherrera"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/soycarlosherrera"><img src={navIcon4} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
