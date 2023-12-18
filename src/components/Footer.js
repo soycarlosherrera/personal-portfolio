6b4c0f1dc08890bad9a8638a5a3b0b8bb18e2a6a
soycarlosherrera <carlos4505@gmail.com>
2023-12-18 02:44:01 -0500

    Completed link navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,10 +17,10 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://linkedin.com/in/soycarlosherrera"><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://github.com/soycarlosherrera"><img src={navIcon4} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/carlosherrerasoy"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://linkedin.com/in/soycarlosherrera/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/soycarlosherrera/"><img src={navIcon4} alt="Icon" /></a>
+              <a href="https://www.facebook.com/carlosherrerasoy/"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/carlosherrerasoy/"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2023. All Rights Reserved</p>
           </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,10 +51,10 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://linkedin.com/in/soycarlosherrera"><img src={navIcon1} alt="" /></a>
-                <a href="https://github.com/soycarlosherrera"><img src={navIcon4} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/carlosherrerasoy"><img src={navIcon3} alt="" /></a>
+                <a href="https://linkedin.com/in/soycarlosherrera/"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/soycarlosherrera/"><img src={navIcon4} alt="" /></a>
+                <a href="https://www.facebook.com/carlosherrerasoy/"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/carlosherrerasoy/"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
